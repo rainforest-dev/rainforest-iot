0e9f3a4af5f907a47c9fa38bd501aabe64cd7f08
Rainforest Cheng <contact@rainforest.tools>
2025-01-28 11:44:31 +0800

    feat: Add helper Docker configuration and bookmarks for homepage module

--- modules/homepage/main.tf
+++ modules/homepage/main.tf
@@ -12,13 +12,34 @@ resource "docker_volume" "configuration" {
   name = "homepage_configuration"
 }
 
+resource "docker_image" "helper" {
+  name         = "helper"
+  keep_locally = false
+  build {
+    context    = "./modules/homepage"
+    dockerfile = "dockerfile.helper"
+  }
+}
+
+resource "docker_container" "helper" {
+  image    = docker_image.helper.image_id
+  name     = "helper"
+  must_run = false
+
+  volumes {
+    container_path = "/data"
+    volume_name    = docker_volume.configuration.name
+  }
+}
+
 resource "docker_image" "homepage" {
   name = "ghcr.io/gethomepage/homepage:latest"
 }
 
 resource "docker_container" "homepage" {
-  image = docker_image.homepage.image_id
-  name  = "homepage"
+  depends_on = [docker_container.helper]
+  image      = docker_image.homepage.image_id
+  name       = "homepage"
   ports {
     internal = 3000
     external = 80
